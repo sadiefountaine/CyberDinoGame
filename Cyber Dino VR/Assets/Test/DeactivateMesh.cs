9bd7e1fc0af6f3a4587ce2c7e6550a5a97e8aebd
Kirt Hullinger <kirt.jh@gmail.com>
2017-04-25 01:37:16 -0600

    Custom VR Controller Version 1

---
+++ Cyber Dino VR/Assets/Test/DeactivateMesh.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivateMesh : MonoBehaviour {
+
+	private MeshRenderer _Renderer;
+
+	void Start () {
+		StartCoroutine ("DeactivateMeshRenderer");
+	}
+		
+	IEnumerator DeactivateMeshRenderer (){
+		yield return new WaitForSeconds (.5f);
+		print ("StartMeshRender");
+		_Renderer = GetComponent<MeshRenderer> ();
+		_Renderer.enabled = false;
+	}
+}
---
+++ Cyber Dino VR/Assets/Test/Make_Taco.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Make_Taco : MonoBehaviour {
+
+    public GameObject prefab;
+    public Transform spawnPoint;
+
+
+    void Update () {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+
+        Instantiate(prefab, spawnPoint.position, Quaternion.identity);
+
+        }
+	}
+}
